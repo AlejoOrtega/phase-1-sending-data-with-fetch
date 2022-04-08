c2736c6220afa03e3d2a742fa290215bc9d21945
Alejandro <ajrevollo@uninorte.edu.co>
2022-04-08 12:03:47 -0400

    commit tree 180bb615c6e27a95dd54167c04720cd6fa814880

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+const postObj = {
+    name : "Steve",
+    email : "steve@steve.com"
+}
+
+function submitData() {
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(postObj)
+    }).then(response => response.json())
+    .then(data => displayResponse(data))
+    .catch(error => displayError(error.message))
+}
+
+function displayResponse(data){
+    let main = document.createElement("p");
+    main.innerHTML=`<h1>${data.name}</h1>
+    <h2>${data.email}</h2>
+    <h3>${data.id}</h3>`
+    document.querySelector('body').appendChild(main)
+}
+
+function displayError(data){
+    let main = document.createElement("p");
+    main.innerHTML=`<h1>${data}</h1>`
+    document.querySelector('body').appendChild(main)
+}
+
+submitData()
